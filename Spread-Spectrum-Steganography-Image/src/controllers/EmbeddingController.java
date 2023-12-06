e2545c2581fece33b91e9a53c1dc10468cb4a7fd
Nguyễn Xuân Hoàng <nguyenxuanhoang02042001@gmail.com>
2023-12-06 12:30:51 +0700

    check oke

--- Spread-Spectrum-Steganography-Image/src/controllers/EmbeddingController.java
+++ Spread-Spectrum-Steganography-Image/src/controllers/EmbeddingController.java
@@ -26,7 +26,14 @@ public class EmbeddingController {
          
         StringBuilder binaryMessage = new DataConversion().stringToBinary(message);
         StringBuilder messageSpreading = new Spreader().spreading(binaryMessage);
-        StringBuilder result = new Modulation().modulate(messageSpreading, key);
+        StringBuilder result = new Modulation().modulate(messageSpreading, key);        
+        
+        FileHandler filehander = new FileHandler();
+        filehander.log("EMB");
+        filehander.log(binaryMessage.toString());
+        filehander.log(messageSpreading.toString());
+        filehander.log(result.toString());
+        
         result.append(new Delimiter("#").getDelimiter());
         int[] bits = new DataConversion().binaryToBits(result);
         BufferedImage image = new FileHandler().getImage(imagePath);
@@ -74,49 +81,4 @@ public class EmbeddingController {
         return new FileHandler().saveImage(stegoImage, stegoPath);
     }
 
-//    public boolean generateStegoImageUTF8() throws IOException {
-//
-//        FileHandler filehander = new FileHandler();
-//        String message = filehander.getMessageUTF8(messagePath);
-//        StringBuilder binaryMessage = new DataConversion().stringToBinary(message);
-//        StringBuilder messageSpreading = new Spreader().spreading(binaryMessage);
-//        StringBuilder result = new Modulation().modulate(messageSpreading, key);
-//
-//        filehander.log("EMB");
-//        filehander.log(binaryMessage.toString());
-//        filehander.log(messageSpreading.toString());
-//        filehander.log(result.toString());
-//
-//        result.append(new Delimiter("#").getDelimiter());
-//        int[] bits = new DataConversion().binaryToBits(result);
-//        BufferedImage image = new FileHandler().getImage(imagePath);
-//        BufferedImage stegoImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
-//        int[] pixels = image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
-//        int[] stegoPixels = new int[pixels.length];
-//        int bitIndex = 0;
-//        for (int i = 0; i < pixels.length; i++) {
-//            int pixel = pixels[i];
-//            int red = (pixel >> 16) & 0xff;
-//            int green = (pixel >> 8) & 0xff;
-//            int blue = (pixel) & 0xff;
-//            int alpha = (pixel >> 24) & 0xff;
-//            if (bitIndex < bits.length) {
-//                red = new LeastSignificantBit(red, bits[bitIndex]).generate();
-//                bitIndex++;
-//            }
-//            if (bitIndex < bits.length) {
-//                green = new LeastSignificantBit(green, bits[bitIndex]).generate();
-//                bitIndex++;
-//            }
-//            if (bitIndex < bits.length) {
-//                blue = new LeastSignificantBit(blue, bits[bitIndex]).generate();
-//                bitIndex++;
-//            }
-//            int stegoPixel = (alpha << 24) | (red << 16) | (green << 8) | blue;
-//            stegoPixels[i] = stegoPixel;
-//        }
-//        stegoImage.setRGB(0, 0, image.getWidth(), image.getHeight(), stegoPixels, 0, image.getWidth());
-//
-//        return filehander.saveImage(stegoImage, stegoPath);
-//    }
 }
